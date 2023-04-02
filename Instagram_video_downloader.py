e89e82161bfa94d4cc32526cca75251860117357
Kumud <113181793+KumudTheDude@users.noreply.github.com>
2023-04-03 04:05:07 +0530

    Update Instagram_video_downloader.py
    There was an issue with the previous version this can download multiple files and save the audio as a separate file

--- Instagram_video_downloader.py
+++ Instagram_video_downloader.py
@@ -1,10 +1,14 @@
 import instaloader
-import subprocess
-import time
+import os
+from moviepy.editor import *
+import moviepy.editor as mp
+import tkinter as tk
+from tkinter import filedialog
+from moviepy.editor import VideoFileClip
+
 
 # Create an instance of Instaloader class
 L = instaloader.Instaloader()
-
 # Get post URL from user input
 print(" ")
 print(" ")
@@ -23,30 +27,46 @@ while True:
     print(" ")
     if post_url == "exit":
         break
-
     # Download the post
     try:
         post = instaloader.Post.from_shortcode(L.context, post_url.split("/")[-2])
         L.download_post(post, target='Instagram Video Download')
         print("")
-        print("ocation: D:\VS Python\Instagram Video Download")
+        print("Location: D:\VS Python\Instagram Video Download")
         print("")
-        print("                        Download complete!")
     except instaloader.exceptions.InvalidArgumentException:
         print("Invalid Instagram URL. ❌❌❌")
     except instaloader.exceptions.BadResponseException:
         print("Fetching post metadata failed.❌❌❌")
     except Exception as e:
         print(f"An error occurred:❌❌❌ {e}")
+    print("                        Download complete!")    
 
-        # Open the download directory in File Explorer
+    # Open the download directory in File Explorer
     print("")
     path = r"D:\VS Python\Instagram Video Download"
-    subprocess.Popen(f'explorer "{path}"')    
-    print("")
-    print("✌-----------------------------------------------------------------------------------✌")
-    print("")
-    print("              Download Videos and Reels with the Audio from 𝕴𝖓𝖘𝖙𝖆𝖌𝖗𝖆𝖒  📽️🎶 ")
-    print("")
-    print("")
+    folder_path = r"D:\VS Python\Instagram Video Download"
+    print(" ")
+    print("             🎶🎶 Extracting Audio As a seperate MP3 file 🎶🎶")
+    print(" ")
+# Get all video files from folder
+    video_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith((".mp4", ".mov"))]
+  
+# Extract audio from each video and save as mp3 file
+    for video_path in video_files:
+        clip = mp.VideoFileClip(video_path)
+        audio_path = os.path.splitext(video_path)[0] + '.mp3'
+        clip.audio.write_audiofile(audio_path)
 
+# Open the directory using Windows Explorer         
+    os.startfile(path)    
+    os.startfile(video_path)
+    print("")
+    print("")
+    print("✌-------------------------------------D O N E-------------------------------------------✌")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("Next URL")
+    print("")
